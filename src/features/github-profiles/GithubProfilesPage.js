354fbd1dcf87b5257f91cf0daa5f959cfa9f0bbd
Artsiom Zhalkouski <azhalkouski@gmail.com>
2021-05-25 21:26:40 +0200

    Setup routes for github-profiles feature

---
+++ src/features/github-profiles/GithubProfilesPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { GithubProfilesContainer } from './GithubProfilesContainer';
+
+export function GithubProfilesPage() {
+  return (
+    <GithubProfilesContainer />
+  );
+}
